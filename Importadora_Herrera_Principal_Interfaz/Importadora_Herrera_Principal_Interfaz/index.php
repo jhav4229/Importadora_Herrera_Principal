fed69c4b75e9260ab13561e83a8720aa2aea46cf
Henry Toapanta <35197384+henryhosting@users.noreply.github.com>
2018-01-07 20:34:40 -0500

    Update index.php

--- Importadora_Herrera_Principal_Interfaz/Importadora_Herrera_Principal_Interfaz/index.php
+++ Importadora_Herrera_Principal_Interfaz/Importadora_Herrera_Principal_Interfaz/index.php
@@ -1,8 +1,6 @@
 <?php
 // el archivo autoload inicializa todos lo archivos necesarios para que el framework funcione
 include "core/autoload.php";
-
-
 // cargamos el modulo iniciar.
 $lb = new Lb();
 $lb->loadModule("index");
